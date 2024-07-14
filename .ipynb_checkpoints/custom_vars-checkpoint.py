e4c036665f904b579a3276f5d4598aa93761fda1
Labo <alaborianog@gmail.com>
2024-07-14 13:10:59 -0500

    Update extraer iamges

---
+++ .ipynb_checkpoints/custom_vars-checkpoint.py
@@ -0,0 +1,3 @@
+url = "https://operaciones.sisfoh.gob.pe:450/cse/"   
+url2024 = "http://159.112.140.62:8140/cse/"
+exe_tes = r"C:\Users\alaboriano\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
